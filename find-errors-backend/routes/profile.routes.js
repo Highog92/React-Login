0c736ed6f090b347914a70b0982e540eaa3ae243
Highog92 <Madsserritslev@gmail.com>
2024-01-19 09:03:55 +0100

    asdasd

--- find-errors-backend/routes/profile.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { fetchUserData } = require("../controllers/profile.controller");
-const { verifyToken } = require("../middleware/auth.middleware");
-const router = express.Router();
-
-// Route to fetch user profile info
-router.get("/profile", verifyToken, fetchUserData);
-
-module.exports = router;
--- find-errors-backend/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-const user = require("../controllers/user.controller");
-let router = require("express").Router();
-
-router.post("/sign-up", user.registerUser);
-router.post("/sign-in", user.signInUser);
-router.post("/refresh-token", user.refreshToken);
-
-module.exports = router;
